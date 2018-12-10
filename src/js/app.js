3a2efd0d120570b4294c205f207d771bbe5544cc
programer10 <programer10@ucione.local>
2018-12-10 21:29:13 +0100

    Header

--- src/js/app.js
+++ src/js/app.js
@@ -10,16 +10,54 @@ var handleLogin = function (){
 			errorTemplate: '<span></span>'
 		}).validate();
 	if($(this).parsley().isValid()){
-		var data = $(this).serialize();
-		console.log(data);
-	}
+		//var data = $(this).serialize();
+		var data = $(this).serializeArray();
+		data = JSON.stringify(data); 
+		$.ajax({
+			type: "GET",
+			url: "data/login.json",
+			data: data,
+			beforeSend: function(){
+				$('.alert').remove();
+			},
+			success: function(result){
+				if(result.isSuccess){
+					//korisnik se uspiješno autorizirao
+					location.assign("http://localhost:3000/dashboard.html");
+			  } else {
+					var alertError = alertBox('danger', 'Korisnički podatci su neispravni. Pokušajte ponovno!');
+					$('.modal-body').prepend(alertError);
+					$('#email').val('');
+					$('#password').val('');
+				}
+			},
+			error: function(){
+				
+			},
+			complete: function(){
+				setTimeout(function(){
+					$('.alert').remove();
+				}, 5000);
+			}
+			
+		});
+		}
 	});
+	function alertBox(type, message){
+	var html = '';
+	html += '<div class="alert alert-' + type + ' fade show" role="alert">'
+	html += message;
+	html += '</div>';
+	
+	return html;
+}
 };
+
 
 var App = function(){
 	return{
 		init: function(){
-			handleLogin();
+			handleLogin(); // Jedna komponenta //
 		}
 	}
 }(); 
